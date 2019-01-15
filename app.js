cb728779bdbb3f90c2993e14b32759a20c7bf13d
bahaaEldeen <bahaaeldeen1999@gmail.com>
2019-01-15 21:54:11 +0200

    added filterAll test on bootstrap

--- app.js
+++ app.js
@@ -4,22 +4,29 @@
 
 */ 
 
-function filter(arr,animateCss){
+function filter(arr, filterAll,animateCss){
     function show(elem,style){
         elem.style.display = "block";
         if( style ){
-            elem.classList.add(style)
+            elem.classList.add(style);
+           
         }else{
             elem.style.opacity = "1";
+           
         }
+        
     }
     function showAll(elemsObj,style){
         for( let elems of elemsObj ){
-            for(let elem of elems.elemsArr){
+            let arr = elems.elemsArr;
+            for(let elem of arr){
+               
                 if( style ){
                     show(elem,style)
+                   
                 }else{
                     show(elem);
+                    
                 }
             }
         }
@@ -36,7 +43,7 @@ function filter(arr,animateCss){
     function hideAll(elemsObj,style){
         for( let elems of elemsObj ){
             let arr = elems.elemsArr;
-            //console.log(arr)
+           
             for(let elem of arr ){
                 if( style ){
                     hide(elem,style)
@@ -58,7 +65,14 @@ function filter(arr,animateCss){
            } 
             );
     }
-
+    if(filterAll){
+        let filterAllBtn = document.getElementById(filterAll);
+        filterAllBtn.addEventListener('click',(e)=>{
+            e.preventDefault()
+            showAll(elements,animateCss)
+        })
+    }
+  
 
     for(let button of buttons){
         button.addEventListener('click',(e)=>{
@@ -86,4 +100,4 @@ function filter(arr,animateCss){
 
 
 }
-//filter( ['filter1','filter2','filter3'] , 'zoomIn')
+filter( [ 'filter1','filter2','filter3'], 'filterAll' , 'zoomIn' )
